2d5248800dde4db40d9d8b09b9101cfd6f636aaa
Murat Kağan <98017089+hantheemp@users.noreply.github.com>
2024-02-10 16:00:44 +0300

    Time: 318 ms (6.96%) | Memory: 44.9 MB (86.35%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+
+		for (int index = 0; index < nums.length; index++) {
+
+			List<Integer> sumLeft = new ArrayList<Integer>();
+			List<Integer> sumRight = new ArrayList<Integer>();
+			int resLeft = 0;
+			int resRight = 0;
+
+			for (int i = 0; i < index; i++) {
+				resLeft += nums[i];
+			}
+
+			for (int i = index + 1; i < nums.length; i++) {
+				resRight += nums[i];
+			}
+
+			if (resLeft == resRight)
+				return index;
+
+		}
+
+		return -1;
+
+	 }
+}
