ff7bee4b3ade83f650a27305a7ad42f281bf9281
Murat Kağan Temel <98017089+hantheemp@users.noreply.github.com>
2025-09-25 14:05:34 +0300

    initial Q&A's

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        for (int index = 0; index < numRows; index++){
+
+            List<Integer> row = new ArrayList<>();
+
+            for (int idx = 0; idx <= index; idx++){
+
+                if (idx == 0 || idx == index){
+                    row.add(1);
+                    continue;
+                }
+                else {
+                    // Get previous rows previous and next elements, add them up as row.
+                    row.add(result.get(index - 1).get(idx - 1) + result.get(index - 1).get(idx));
+                }
+
+            }
+
+            result.add(row);
+
+        }
+
+        return result;
+
+    }
+}
