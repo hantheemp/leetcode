f7a60945aa5c386fbd4f545d6e99b45e50dd3397
Murat Kağan Temel <98017089+hantheemp@users.noreply.github.com>
2025-10-15 21:24:41 +0300

    Time: 0 ms (100.00%) | Memory: 40.4 MB (97.66%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.java
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -1,23 +1,17 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
 
-		String result = "";
+		int num = columnNumber;
 
-		Map<Integer, Character> map = new HashMap<Integer, Character>();
+		StringBuilder sb = new StringBuilder();
 
-		for (int index = 65; index < 91; index++) {
-			char character = (char) index;
-			map.put(index - 65, character);
+		while (num != 0) {
+			num--;
+			sb.append((char) ('A' + num % 26));
+			num /= 26;
 		}
 
-		while (columnNumber != 0) {
-			columnNumber -= 1;
-			int remainder = columnNumber % 26;
-			result = map.get(remainder) + result;
-			columnNumber /= 26;
-		}
-
-		return result;
+		return sb.reverse().toString();
 
 	}
 }
