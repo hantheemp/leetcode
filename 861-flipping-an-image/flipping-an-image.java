e19b5fc9d977385aacb32e60ab6f5927d49dd7a3
Murat Kağan Temel <98017089+hantheemp@users.noreply.github.com>
2025-10-19 01:05:49 +0300

    Time: 1 ms (35.76%) | Memory: 44.8 MB (26.58%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+
+		int numberOfRows = image.length;
+		int numberOfColumns = image[0].length;
+
+		int result[][] = new int[numberOfRows][numberOfColumns];
+
+		for (int index = 0; index < numberOfRows; index++) {
+			for (int i = 0; i < numberOfColumns; i++) {
+				result[index][i] = image[index][numberOfColumns - 1 - i];
+			}
+		}
+
+		for (int index = 0; index < numberOfRows; index++) {
+			for (int i = 0; i < numberOfColumns; i++) {
+				if (result[index][i] == 1)
+					result[index][i] = 0;
+				else
+					result[index][i] = 1;
+			}
+		}
+
+		return result;
+
+	}
+}
